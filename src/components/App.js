830cff0a7bcdff5a3c644810cf5df3d5661b888a
DESKTOP-9HSRLI7\admin <tejaswibhokare17@gmail.com>
2023-07-27 16:12:59 -0400

    imported libraries and class component in editor component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,49 @@
-
+import React, { useState } from "react";
+import Editor from "./Editor";
 
 function App() {
+  const [html, setHtml] = useState('')
+  const [css, setCss] = useState('')
+  const [js, setJs] = useState('')
+
   return (
-    'Hello World'
-  );
+    <>
+      <div >
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
+
+      </div>
+
+      <div className="pane"></div>
+      <iframe
+        title="output"
+        sandbox="allow-scripts"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+      />
+
+    </>
+  )
+
 }
 
 export default App;
